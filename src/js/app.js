2b4fe6ec7ccab414c2e616cee0e6a82921ea68ee
Ornela <ornelamarini1983@gmail.com>
2021-10-15 11:03:37 -0300

    modificaciones 15/10

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,35 @@ document.addEventListener('DOMContentLoaded', function(){
         iniciarApp()
 });
 function iniciarApp(){
+    //Fijar Navegación
+    navegacionFija();
     //Activa el Menú
     eventListeners();
     //Resalta el link actual que se presiona
     activeLink()
     
 }
+function navegacionFija(){
+    const barra = document.querySelector('.header');
+    const principal = document.querySelector('.principal')
 
+    window.addEventListener('scroll', function(){
+        //console.log(principal.getBoundingClientRect());
+
+        if ( principal.getBoundingClientRect().bottom < 0 ){
+            barra.classList.add('fijo');
+        }else{
+            barra.classList.remove('fijo');
+        }
+    });
+}
 function eventListeners(){
     const mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.addEventListener('click', ()=>{
         const navegacion = document.querySelector('.navegacion');
         //console.log(navegacion)
         navegacion.classList.toggle('mostrar');
-    })
+    });
 };
 
 function activeLink(){
